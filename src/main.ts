4065695f11c9dab62f9adee638b75c522a2c1ed1
Lidia Haile <lidiahaile@gmail.com>
2024-04-04 14:50:05 +0100

    html layout and main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,21 @@
 import "./styles/style.scss";
 
+import {pokemonArray}  from "./data/pokemon.ts";
 
+const cardsContainer = document.querySelector<HTMLDivElement>(".card-container")
+const eachCard = document.querySelector<HTMLDivElement> (".card")
+const cardImg = document.querySelector<HTMLImageElement>("card__image")
+const cardHeading  = document.querySelector<HTMLImageElement>("card__heading")
+const cardText  = document.querySelector<HTMLImageElement>("card__text")
+
+
+if(!cardsContainer || !eachCard|| !cardImg || !cardHeading || !cardText){
+    throw new Error ("issue with selectors");
+}
+
+
+pokemonArray.forEach((pokemon) => {
+   cardImg.src = pokemonArray.pokemon.sprite;
+   
+   
+});
